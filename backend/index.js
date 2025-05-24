c2aa85982ed60dac142722b543c4cc54c3d2bca3
Kalyan Badhavath <badhavathkalyan08@gmail.com>
2025-05-24 22:01:24 +0530

    Final commit of backend

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', router);
 
-const PORT = process.env.PORTS || 8080;
+const PORT =  8080;
 const url=process.env.MONGODB_URI
 
 app.listen(PORT, () => {
